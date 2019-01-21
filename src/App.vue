d4be38f621b82790e57dbf64a42c2e4fb20dd09e
김정윤 <skyepodium@gmail.com>
2019-01-21 20:49:17 +0900

    redirect, router-link

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <tool-bar />
     <router-view />
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue'
 
 export default {
   name: 'App',
+  components: {
+    ToolBar,
+  },
 }
 </script>
 
