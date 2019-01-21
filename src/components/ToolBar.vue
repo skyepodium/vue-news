44c36945e8c2e5fc90aace4388d2488ba605d344
김정윤 <skyepodium@gmail.com>
2019-01-21 21:18:02 +0900

    vue-rotuer mode: 'history' to remove hashcode in url

--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -18,7 +18,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .header {
   color: white;
   background-color: #42b883;
---
+++ views/UserView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    userview
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
